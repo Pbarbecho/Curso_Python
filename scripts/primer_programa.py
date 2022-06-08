dafa92de73dc07844270183746946b288e68c6b3
Pablo Barbecho <58553873+Pbarbecho@users.noreply.github.com>
2022-06-08 21:48:16 +0200

    Add files via upload

---
+++ scripts/primer_programa.py
@@ -0,0 +1,5 @@
+print("Hola Bienvenidos: Este es un script de python\n")
+
+print("1. Mis datos personales son: Pablo Barbecho")
+print("2. Vivo en: Cuenca - Ecuador")
+print("3. Imparto clases en: Primaria")
